5fb84e95d100cb3f6a2f0441534c187d16521f1b
lirangazit <47790066+lirangazit@users.noreply.github.com>
2019-03-18 16:53:15 -0400

    Add files via upload
    Edited to make sure there is always data in the input buffer

--- BumpsWheelDropsSensorsRoomba.m
+++ BumpsWheelDropsSensorsRoomba.m
@@ -14,33 +14,51 @@ WheDropLeft = nan;
 WheDropCaster = nan;
 BumpFront = nan;
 
+BmpWheDrps = '00000000';
+
 try
 
 %Flush Buffer    
-% N = serPort.BytesAvailable();
-% while(N~=0) 
-% fread(serPort,N);
-% N = serPort.BytesAvailable();
-% end
+flushinput(serPort);
 
 warning off
 global td
 
 fwrite(serPort, [142 7]);
-pause(0.5);
-BmpWheDrps = dec2bin(fread(serPort, 1),8);
-BumpRight = bin2dec(BmpWheDrps(end));
-BumpLeft = bin2dec(BmpWheDrps(end-1));
-WheDropRight = bin2dec(BmpWheDrps(end-2));
-WheDropLeft = bin2dec(BmpWheDrps(end-3));
-WheDropCaster = bin2dec(BmpWheDrps(end-4));
-BumpFront=(BumpRight*BumpLeft);
-if BumpFront==1
-    BumpRight =0;
-    BumpLeft =0;
+while serPort.BytesAvailable==0
+%     pause(0.1);
 end
+
+BmpWheDrps = dec2bin(fread(serPort, 1),8);
+% BumpRight = bin2dec(BmpWheDrps(end));
+% BumpLeft = bin2dec(BmpWheDrps(end-1));
+% WheDropRight = bin2dec(BmpWheDrps(end-2));
+% WheDropLeft = bin2dec(BmpWheDrps(end-3));
+% WheDropCaster = bin2dec(BmpWheDrps(end-4));
+% BumpFront=(BumpRight*BumpLeft);
+% if BumpFront==1
+%     BumpRight =0;
+%     BumpLeft =0;
+% end
 
 pause(td)
 catch
-    disp('WARNING:  function did not terminate correctly.  Output may be unreliable.')
+    disp('WARNING:  BumpsWheelDropsSensorsRoomba function did not terminate correctly.  Output may be unreliable.')
+end
+
+try
+    BumpRight = bin2dec(BmpWheDrps(end));
+    BumpLeft = bin2dec(BmpWheDrps(end-1));
+    WheDropRight = bin2dec(BmpWheDrps(end-2));
+    WheDropLeft = bin2dec(BmpWheDrps(end-3));
+    WheDropCaster = bin2dec(BmpWheDrps(end-4));
+    BumpFront=(BumpRight*BumpLeft);
+    if BumpFront==1
+        BumpRight =0;
+        BumpLeft =0;
+    end
+catch
+    disp('WARNING: Error here')
+end
+
 end
