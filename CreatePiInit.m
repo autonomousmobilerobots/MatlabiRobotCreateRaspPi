c97e238d60379b65bc8d404196a20ab8d8e5aca5
lirangazit <47790066+lirangazit@users.noreply.github.com>
2020-01-14 09:18:49 -0500

    Changes to add Optitrack overhead localization system

--- CreatePiInit.m
+++ CreatePiInit.m
@@ -1,15 +1,19 @@
-function port = CreatePiInit(remoteHost)
+function ports = CreatePiInit(remoteHost)
 %port = CreatePiInit(remoteHost)
-% 'port' is the tcp/ip port for commanding create
 %
-% This file initializes tcp/ip port for use with iRobot Create
-% remoteHost is the ip address of the beagleboard
-% ex. CreateBeagleInit('192.168.1.141') sets ip = '192.168.1.141'
-% copy this file along with PacketType.m into MatlabToolBoxiRobotCreate
+% This file initializes a NatNet class client to get Optitrack data
+% ports.OL_Client - Optitrack NatNet client
+%
+% This file initializes 3 ports for use with iRobot Create
+% ports.create - tcp port for commands to the create
+% ports.dist - udp port for distance telemetry from the Create
+% ports.tag - udp port for tag telemetry from the Create
+%
+% remoteHost is a string with the name or IP address of the Pi
+% ex. Ports = CreatePiInit('192.168.1.141') or Ports = CreatePiInit('eve') 
 %
 % The tcp/ip server must be running on the Raspberry Pi before running this
-% code.
-%
+% function.
 % If you receive the error "Unsuccessful open: Connection refused: connect"
 % ensure that the server code is running properly on the Raspberry Pi
 %
@@ -18,6 +22,7 @@ function port = CreatePiInit(remoteHost)
 %
 % By: Chuck Yang, ty244, 2012
 % Modified By: Alec Newport, acn55, 2018
+% Liran, 2019, 2020
 
 global td
 td = 0.015;
@@ -26,55 +31,58 @@ CreatePort = 8865; % TCP
 DistPort = 8833; % UDP
 TagPort = 8844; % UDP
 
-% Open SSH connection to the host
-InitSSH_Connection(remoteHost);
+%Init NatNet client and connect to Optitrack server
+ports.OL_Client = Init_OverheadLocClient();
+
+% Open SSH connection to the Create, and start the script
+InitSSH_Connection(remoteHost, './robot');
 % Patience
 pause (3);
 
 % use TCP for control commands and data from the Create
-port.create = tcpip(remoteHost, CreatePort, 'inputbuffersize', 64);
+ports.create = tcpip(remoteHost, CreatePort, 'inputbuffersize', 64);
 
 % use UDP for distance and tag reading
-port.dist = udp(remoteHost, DistPort, 'LocalPort', DistPort);
-port.tag = udp(remoteHost, TagPort, 'LocalPort', TagPort);
+ports.dist = udp(remoteHost, DistPort, 'LocalPort', DistPort);
+ports.tag = udp(remoteHost, TagPort, 'LocalPort', TagPort);
 
-port.dist.ReadAsyncMode = 'continuous';
-set(port.dist,'Timeout',1);
-port.dist.inputbuffersize = 512;
+ports.dist.ReadAsyncMode = 'continuous';
+set(ports.dist,'Timeout',1);
+ports.dist.inputbuffersize = 512;
 
-port.tag.ReadAsyncMode = 'continuous';
-set(port.tag,'Timeout',1);
-port.tag.inputbuffersize = 512;
+ports.tag.ReadAsyncMode = 'continuous';
+set(ports.tag,'Timeout',1);
+ports.tag.inputbuffersize = 512;
 
 warning off
 
 disp('Opening connection to iRobot Create...');
-	fopen(port.create);
+	fopen(ports.create);
 	pause(0.5)
 % udp ports are opened and closed in the tag and dist functions
 
 %% Confirm two way connumication
 disp('Setting iRobot Create to Control Mode...');
 % Start! and see if its alive
-fwrite(port.create,128);
+fwrite(ports.create,128);
 pause(0.1)
 
 % Set the Create in Full Control mode
 % This code puts the robot in CONTROL(132) mode, which means does NOT stop 
 % when cliff sensors or wheel drops are true; can also run while plugged 
 % into charger
-fwrite(port.create,132);
+fwrite(ports.create,132);
 pause(0.1)
 
 % light LEDS
-fwrite(port.create,[139 25 0 128]);
+fwrite(ports.create,[139 25 0 128]);
 
 % set song
-fwrite(port.create, [140 1 1 48 20]);
+fwrite(ports.create, [140 1 1 48 20]);
 pause(0.1)
 
 % sing it
-fwrite(port.create, [141 1])
+fwrite(ports.create, [141 1])
 
 pause(0.1)
 
